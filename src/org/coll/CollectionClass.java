ed521a95c8a87e9c466f40f708e9c4618fd5e06a
karthickmd <karthickmd1998@gmail.com>
2021-12-11 17:32:50 +0530

    firstgit

---
+++ src/org/coll/CollectionClass.java
@@ -0,0 +1,44 @@
+ package org.coll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List li = new ArrayList();
+		li.add("Greens");
+		li.add("OMR");
+		li.add(2345);
+		li.add(2389.883f);
+		li.add(2345);
+
+		System.out.println(li);
+		int size = li.size();
+		System.out.println(size);
+		
+		int indexOf = li.indexOf("Greens");
+		System.out.println(indexOf);
+
+		int lastIndexOf = li.lastIndexOf("OMR");
+		System.out.println(lastIndexOf);
+
+		boolean contains = li.contains(2345);
+		System.out.println(contains);
+
+		li.add("selenium");
+		System.out.println(li);
+		
+		li.set(3, "Automation");
+		System.out.println(li);
+		
+		for (int i = 0; i<4 ; i++) {
+			System.out.println(li.get(i));
+			
+			
+		}
+	}
+
+}
